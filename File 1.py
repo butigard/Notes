273775f75477777322c1a940eeffbedfc659e612
butigard <butigard@fwparker.org>
2018-02-02 09:06:45 -0600

    notes

---
+++ File 1.py
@@ -0,0 +1,7 @@
+for a in range(1,10):
+    for b in range(10):
+        for c in range(10):
+            for d in range(1,10):
+                num = str(a) + str(b) + str(c) + str(d)
+                num_reverse = str(d) + str(c) + str(b) + str(a)
+                print(num, num_reverse)
