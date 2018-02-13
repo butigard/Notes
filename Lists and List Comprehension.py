644a2586e104b6a33ae449ed401b8d663343bd7e
butigard <butigard@fwparker.org>
2018-02-13 13:33:58 -0600

    notes

--- Lists and List Comprehension.py
+++ Lists and List Comprehension.py
@@ -122,3 +122,114 @@ for i in range(len(my_list)):
     my_list[i] **= 2
 
 print(my_list)
+
+print("\n")
+
+# ITERATING A LIST
+# Make a list from 0 to 9
+my_list = []
+for i in range(10):
+    my_list.append(i)
+
+print(my_list)
+
+# print each item in the list using for each
+for item in my_list:
+    print(item)
+
+# adds 10 to each item in the list using iteration
+for i in range(len(my_list)):
+    my_list[i] += 10
+print(my_list)
+
+# make a 2d list that is 10 x 10  [[0,0], [0,1], [0,2]...[9,9]]
+my_list = []
+for i in range(10):
+    for j in range(10):
+        my_list.append([i, j])
+print(my_list)
+
+# print each pair
+for item in my_list:
+    print(item)
+
+# adds 10 to each y item using iteration
+for i in range(len(my_list)):
+    my_list[i][1] += 10
+
+# LIST COMPREHENSION
+# make a list of numbers from 0 to 99 and print it
+my_list = []
+for i in range(100):
+    my_list.append(i)
+print(my_list)
+
+# or we can do it this way
+my_list = [x for x in range(100)]
+print(my_list)
+
+# square every item in the previous list and print it
+for i in range(len(my_list)):
+    my_list[i] **= 2
+print(my_list)
+
+my_list = [x ** 2 for x in my_list]
+print(my_list)
+
+#create the list and square it
+my_list = [x ** 2 for x in range(100)]
+print(my_list)
+
+# alter list to show only odd numbers and print it
+my_list2 = []
+for square in my_list:
+    if square % 2 ==1:
+        my_list2.append(square)
+print(my_list2)
+
+my_list2 = [x for x in my_list if x % 2 == 1]
+print(my_list2)
+
+# alter list to show only numbers 100 to 1000 and print it
+my_list3 = []
+for square in my_list2:
+    if square >= 100 and square <= 1000:
+        my_list3.append(square)
+print(my_list3)
+
+my_list3 = [x for x in my_list2 if x >= 100 and x <= 1000]
+print(my_list3)
+
+# all four at once
+my_list4 = [x ** 2 for x in range(100) if x ** 2 % 2 == 1 and x ** 2 >= 100 and x ** 2 <= 1000]
+print(my_list4)
+
+print("\n")
+
+# list comprehension
+import random
+# [returned_item for iterator in range_or_list filter]
+
+# roll a single die 100 times and put in list
+'''
+my_list = []
+for i in range(100):
+    roll = random.randrange(1, 7)
+    my_list.append(roll)
+print(my_list)
+'''
+
+rolls = [random.randrange(1, 7) for x in range(100)]
+print(rolls)
+
+# roll two dice in pairs 100 times and put in list
+rolls = [[random.randrange(1, 7), random.randrange(1, 7)] for x in range(100)]
+print(rolls)
+
+# create a list of only the doubles from my_rolls
+doubles = [x for x in rolls if x[0] == x[1]]
+print(doubles)
+
+# can we generate a list and filter doubles on single line
+doubles = [x for x in [[random.randrange(1, 7), random.randrange(1, 7)] for x in range(100)] if x[0] == x[1]]
+print(doubles)
