9bfe3685f1c1c3c1787d217f29f4b8b0e84f984f
butigard <butigard@fwparker.org>
2018-02-27 13:45:57 -0600

    notes

--- Searching.py
+++ Searching.py
@@ -24,9 +24,12 @@ file.close()
 
 # Read the file into a array (list)
 file = open('data/villains', 'r')
+
+
 villains = []
 for line in file:
     villains.append(line.strip())
+
 
 print(villains)
 
@@ -38,3 +41,25 @@ while i < len(villains) - 1 and key != villains[i]:
 
 if i < len(villains):
     print("Found", key, "at position", i)
+
+# Binary Searching
+print("\n\n\n\n\n\n")
+key = input("Input a Word: ")
+lower_bound = 0
+upper_bound = len(villains) - 1
+found = False
+
+# loop until we find it
+while lower_bound <= upper_bound and not found:
+    middle_pos = (upper_bound + lower_bound) // 2
+    if villains[middle_pos] < key:
+        lower_bound = middle_pos + 1
+    elif villains[middle_pos] > key:
+        upper_bound = middle_pos - 1
+    else:
+        found = True
+
+if found:
+    print(key, "was found at position", middle_pos)
+else:
+    print(key, "was not found in the list")
