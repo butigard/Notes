1d2131ff4109d2c1f305a9fc927b88319253c2d4
butigard <butigard@fwparker.org>
2018-03-09 08:16:28 -0600

    notes

--- Sorting B.py
+++ Sorting B.py
@@ -29,3 +29,23 @@ for cur_pos in range(len(my_list)):
     my_list[min_pos], my_list[cur_pos] = my_list[cur_pos], my_list[min_pos]
 
 print(my_list)
+
+# Insertion Sort
+
+my_list = [random.randrange(100) for x in range(100)]
+print(my_list)
+
+for key_pos in range(1, len(my_list)):
+    key_value = my_list[key_pos]
+
+    # scan from right to left
+    scan_pos = key_pos - 1
+
+    # loop through until we find one smaller
+    while (scan_pos >= 0) and (my_list[scan_pos] > key_value):
+        my_list[scan_pos + 1] = my_list[scan_pos]
+        scan_pos -= 1
+
+    my_list[scan_pos + 1] = key_value
+
+print(my_list)
