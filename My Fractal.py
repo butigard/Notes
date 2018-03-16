25e03e6575677bf79fb6534037115004354cbb76
butigard <butigard@fwparker.org>
2018-03-16 09:12:12 -0500

    notes

---
+++ My Fractal.py
@@ -0,0 +1,32 @@
+import turtle
+import random
+import math
+
+my_turtle = turtle.Turtle()
+my_turtle.showturtle()
+my_turtle.shape("turtle")
+my_turtle.speed(0)
+my_screen = turtle.Screen()
+my_screen.bgcolor('white')
+my_turtle.width(1)
+my_turtle.fillcolor("grey")
+colors = ["pink", "purple", "blue", "yellow", "green", "orange", "red"]
+
+#------------------------
+
+def recursive_frac(x, y, size, depth, angle):
+    m = 2
+    if depth > 0:
+        my_turtle.pencolor(colors[random.randrange(len(colors))])
+        my_turtle.up()
+        my_turtle.goto(x, y)
+        my_turtle.down()
+        my_turtle.right(angle)
+        my_turtle.forward(size)
+        recursive_frac(x, y, size / 1.003, depth - 1, angle)
+
+
+recursive_frac(0, 0, 300, 3000, 1)
+
+
+my_screen.exitonclick() # end of program
